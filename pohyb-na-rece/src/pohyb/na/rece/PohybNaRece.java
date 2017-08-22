91fd97d02b2a16ba3de1a2cce0beb4173b98b898
Student <Student@192.168.0.213>
2017-08-22 19:20:37 +0200

    počáteční nastavení ...

---
+++ pohyb-na-rece/src/pohyb/na/rece/PohybNaRece.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pohyb.na.rece;
+
+/**
+ *
+ * @author Student
+ */
+public class PohybNaRece {
+
+    /** zadání: vytvořit třídu ship (2-3 atr.), z ní potomky battleShip, tradeShip;
+     * dále třídu River, kde v ArrayListu budou jedn. instance lodí + následně
+     * budeme zkoušet různé metody na třídě Collections
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ pohyb-na-rece/src/pohyb/na/rece/TradeShip.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pohyb.na.rece;
+
+/**
+ *
+ * @author Student
+ */
+public class TradeShip {
+    
+}
